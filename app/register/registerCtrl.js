3feaabf807b81e2a354f189f6cc04cd30efc8ca6
kiran paturi <kpaturi@smarttuition.com>
2016-07-30 10:28:34 +0530

    added code

--- app/register/registerCtrl.js
+++ app/register/registerCtrl.js
@@ -9,12 +9,57 @@ angular.module("register")
         };
         $scope.countries = [{
                 "name": "India",
-                "code": "IN"
+                "code": "IN",
+                "phonecode": "+91"
         },
             {
                 "name": "United States",
-                "code": "US"
+                "code": "US",
+                "phonecode": "+1"
                           }];
+
+        var states = [{
+                "stateName": "Telangana",
+                "stateCode": "TG",
+                "countryCode": "IN"
+            },
+
+            {
+                "stateName": "New York",
+                "stateCode": "NY",
+                "countryCode": "US"
+            },
+            {
+                "stateName": "Andhra Pradesh",
+                "stateCode": "AP",
+                "countryCode": "IN"
+            },
+
+            {
+                "stateName": "Las Vegas",
+                "stateCode": "LS",
+                "countryCode": "US"
+            }
+                     ];
+        $scope.phonePattern = "/^\d+$/";
+        $scope.loadStates = function () {
+            if ($scope.selectedCountry.code == "US") {
+                $scope.phonePattern = "^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$"
+            } else {
+                $scope.phonePattern = "/^\d+$/"
+            }
+
+            $scope.states = [];
+            angular.forEach(states, function (item) {
+                if (item.countryCode == $scope.selectedCountry.code) {
+                    $scope.states.push(item);
+                }
+            });
+        };
+        $scope.groupedStates = states;
+
+
+
         $scope.checkFields = function () {
             if ($scope.registerUser.firstName && $scope.registerUser.lastName && $scope.registerUser.age && $scope.registerUser.gender && $scope.registerUser.terms) {
                 $scope.enableButton = true;
